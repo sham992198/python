f4de5d377c649fa0d513b04dae991d720d8345a9
sham992198 <kolekarsham9921@gmail.com>
2025-07-13 23:03:56 +0530

    solve problem

---
+++ 4problemsolving.py
@@ -0,0 +1,30 @@
+# to add two numbers
+a = 33
+b = 76
+c = a+b
+print(c)
+
+# to find remainder when number is divide by z
+a = 37
+b = 5
+print(a%b)
+
+# check the type of variable assinged using input function
+a = input("Enter :")
+print(type(a))
+
+# use comparisan operator to find out given variable is greater than b or not.a=34,b=80
+a = 35
+b= 80
+print("a is greater than b:",a>b)
+print("a is not greater:",a<b)
+
+# to find as average of two numbers enter by user
+a =int(input("Enter:"))
+b =int(input("Enter:"))
+print("Avg is:",(a+b)/2)
+
+#to calc the square of no enter by user
+a = int(input("Enter:"))
+print("square of no:",a*a)
+

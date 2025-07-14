28285bb57f3597441c568aea5d69d703d088aa62
sham992198 <kolekarsham9921@gmail.com>
2025-07-14 10:51:01 +0530

    explain string

---
+++ 5String.py
@@ -0,0 +1,28 @@
+# Slicing positive
+# a = "sham"
+# print(a[0:3])
+# print(a[0:4])
+# print(a[:])
+
+# Negative slicing
+
+# print(a[-4])
+# print(a[-4:-1])
+
+# Slicing with skip value
+# x = "amaging"
+# x[0:6:2]
+
+# String Function
+name = "sham"
+print(len(name))
+print(name.startswith("sh"))
+print(name.endswith("sh"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+
+# Escape sequence char
+x = " sham is good boy \n not a bad \n \"boy\""
+print(x)
+

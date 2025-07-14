35cf31c93410bbcc6f469fd1890e643ad890d51d
sham992198 <kolekarsham9921@gmail.com>
2025-07-14 13:26:57 +0530

    List function explain

---
+++ 7_List.py
@@ -0,0 +1,19 @@
+l = ["Apple","orange","false",33,"ram"]
+print(l[0])
+
+# perform function in list
+x = [11,3,44,65,76,34,35,667,7]
+x.sort()
+print(x)
+
+x = [11,3,44,65,76,34,35,667,7]
+x.reverse()
+print(x)
+
+x = [11,3,44,65,76,34,35,667,7]
+x.pop(5)
+print(x)
+
+x = [11,3,44,65,76,34,35,667,7]
+x.insert(3,90)
+print(x)

4d2b75d75601a22be1534a155913e8edd00ed122
sham992198 <kolekarsham9921@gmail.com>
2025-07-15 13:27:15 +0530

    Expalain dictionary basic

---
+++ 10_dictionary.py
@@ -0,0 +1,13 @@
+marks ={
+    "sham":56,
+    "ram":99,
+    100:"ajay"
+}
+print(marks)
+print(marks["sham"])
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"sham":87,"rani":20})
+print(marks.get("sham"))
+print(marks)
